e70ee7c13a097f12eb766040f01887c1bbe77eb2
Anna Elizabeth Woodard <awoodard@nd.edu>
2025-08-07 13:58:39 -0500

    add recipe

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="prima",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Polygenic Risk and Imaging Multimodal Assessment",
+    author="Olopade and Huo Labs",
+    license="MIT",
+)
